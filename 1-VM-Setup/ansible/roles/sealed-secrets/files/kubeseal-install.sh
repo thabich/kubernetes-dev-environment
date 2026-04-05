4d340a8b993d053c4248f51ba510f85a5a63cac6
Thorsten Habich <thorsten.habich@findichgut.net>
2026-04-05 18:03:50 +0300

    remove sealed secrets as it has to be installed by ansible

---
+++ 1-VM-Setup/ansible/roles/sealed-secrets/files/kubeseal-install.sh
@@ -0,0 +1,12 @@
+# Fetch the latest sealed-secrets version using GitHub API
+KUBESEAL_VERSION=$(curl -s https://api.github.com/repos/bitnami-labs/sealed-secrets/tags | jq -r '.[0].name' | cut -c 2-)
+
+# Check if the version was fetched successfully
+if [ -z "$KUBESEAL_VERSION" ]; then
+    echo "Failed to fetch the latest KUBESEAL_VERSION"
+    exit 1
+fi
+
+curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
+tar -xvzf kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
